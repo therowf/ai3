4b5b3564ebe3d10ac85fe4458a908041601016d3
RAKIB HASAN <rakibhasan@RAKIBs-iMac.local>
2020-04-02 21:40:15 +0600

    c

---
+++ build/precache-manifest.4ce1b3995818bcb0fafd63c063da470c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432d3c70211fe35c9cb5bd0a7a288ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa23d9ee726a3de63be8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6829618cf22f87bd908b",
+    "url": "/static/js/2.2829b281.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2829b281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa23d9ee726a3de63be8",
+    "url": "/static/js/main.1f8aab6f.chunk.js"
+  },
+  {
+    "revision": "c856eeb0c0545fa9bab4",
+    "url": "/static/js/runtime-main.2e50cf14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
