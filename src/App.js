4d3b190bcb34e30cfe1d7ff5286feff00f3659e1
RAKIB HASAN <rakibhasan@RAKIBs-iMac.local>
2020-04-02 20:20:00 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SpeechRecognizer } from 'react-speech-recognizer-component';
+import Speech from 'react-speech';
+import { DictateCheckbox } from 'react-dictate-button';
+class App extends React.Component{
+ constructor(porps){
+   super(porps)
+   this.state = {isauto:false}
+   this.handleStart = this.handleStart.bind(this)
+   this.handleStop = this.handleStop.bind(this)
+ }
+  handleStart(){
+    this.setState((state, props) => {
+      return {isauto: true};
+    });
+}
 
-function App() {
+
+  handleStop(e){
+    this.setState((state, props) => {
+      return {isauto: false};
+    });
+}
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
+   <header className="App-header">
+   <DictateCheckbox
+    className="my-dictate-checkbox"
+    grammar="#JSGF V1.0; grammar districts; public <district> = Redmond | Bellevue;"
+    onDictate={ this.handleDictate }
+    onProgress={ this.handleProgress }
+  >
+    Start/stop
+  </DictateCheckbox>
+      <SpeechRecognizer
+        startSpeechRecognition={this.state.isauto}
+        onError={this.onError}
+        continuous={false}
+      >
+          {({status, results, formattedResults, transcripts, error}) => {
+            return (
+              <>
+                {transcripts && transcripts.length && <p>{transcripts.join(', ')}</p>}
+              </>
+            );
+          }}
+      </SpeechRecognizer>
+      <button onClick={this.handleStart}>start</button>
+      <button onClick={this.handleStop}>stop</button>
+      
+        <h3 id="nod">AutoSpeeker</h3>
+        <Speech text="This will work if you want, but please be wait to get more." />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,5 +71,5 @@ function App() {
     </div>
   );
 }
-
+}
 export default App;
